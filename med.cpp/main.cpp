b7fe0470408afc4a4779d4790f5697792d1dd645
Anthony Capobianco <anth@live.be>
2017-11-11 04:42:16 +0100

    Initial Commit

---
+++ med.cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  med.cpp
+//
+//  Created by Anthony Capobianco on 11/11/2017.
+//  Copyright © 2017 Anthony Capobianco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
